43ed3ce233fc0aa2a28ca61c6ab5cb7b3cfeba6f
Brian Hannah <bhanna200@caledonian.ac.uk>
2012-09-16 21:56:36 +0100

    Initializing Game Framework

--- GP2/main.cpp
+++ GP2/main.cpp
@@ -2,30 +2,56 @@
 #ifdef WIN32
 //if so include the header for windows applications
 #include <Windows.h>
+#include "GameApplication.h"
+
 #endif
 
 #include "Win32Window.h"
 
 //Check to see if we are on windows
-#ifdef WIN32
+//#ifdef WIN32
 int WINAPI WinMain(HINSTANCE hInstance, 
 					HINSTANCE hPrevInstance, 
 					LPSTR lpCmdLine,
 					int nCmdShow)
-#else
-//we are on some other platform
-int main(int argc, char **argv)
-#endif
 {
-	CWin32Window * pWin=new CWin32Window();
-	pWin->init(TEXT("My First Window"),860,640,false);
+	CGameApplication * pApp=new CGameApplication();
 
-	while(pWin->running()){
-		pWin->checkForWindowMessages();
+	if (!pApp->init())
+	{
+		if (pApp)
+		{
+			delete pApp;
+			pApp = NULL;
+			return 1;
+		}
 	}
 
-	delete pWin;
+	pApp->run();
 
+	if (pApp)
+	{
+		delete pApp;
+		pApp=NULL;
+	}
 	return 0;
 }
+
+
+//#else
+////we are on some other platform
+//int main(int argc, char **argv)
+//#endif
+//{
+//	CWin32Window * pWin=new CWin32Window();
+//	pWin->init(TEXT("My First Window"),860,640,false);
+//
+//	while(pWin->running()){
+//		pWin->checkForWindowMessages();
+//	}
+//
+//	delete pWin;
+//
+//	return 0;
+//}
 
